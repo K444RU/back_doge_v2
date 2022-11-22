3b1e4a832b5fa5e68274b41164b089a2e62e0db1
GoodVib3s <vladislav.jagur@gmail.com>
2022-11-22 21:41:28 +0200

    Added informationd to database and filled data_create.sql file.

--- database/data_create.sql
+++ database/data_create.sql
@@ -1,69 +1,135 @@
-INSERT INTO public.role (id, type) VALUES (DEFAULT, 'walker');
-INSERT INTO public.role (id, type) VALUES (DEFAULT, 'owner');
+INSERT INTO public.role (id, type)
+VALUES (DEFAULT, 'walker');
+INSERT INTO public.role (id, type)
+VALUES (DEFAULT, 'owner');
 
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Tallinn');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Tartu');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Narva');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Pärnu');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Kohtla-Järve');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Viljandi');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Maardu');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Rakvere');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Kuressaa');
-INSERT INTO public.city (id, name) VALUES (DEFAULT, 'Sillamäe');
+INSERT INTO public.city (id, name)
+VALUES (DEFAULT, 'Tallinn');
+INSERT INTO public.city (id, name)
+VALUES (DEFAULT, 'Tartu');
+INSERT INTO public.city (id, name)
+VALUES (DEFAULT, 'Narva');
+INSERT INTO public.city (id, name)
+VALUES (DEFAULT, 'Pärnu');
+INSERT INTO public.city (id, name)
+VALUES (DEFAULT, 'Kohtla-Järve');
+INSERT INTO public.city (id, name)
+VALUES (DEFAULT, 'Viljandi');
+INSERT INTO public.city (id, name)
+VALUES (DEFAULT, 'Maardu');
+INSERT INTO public.city (id, name)
+VALUES (DEFAULT, 'Rakvere');
+INSERT INTO public.city (id, name)
+VALUES (DEFAULT, 'Kuressaa');
+INSERT INTO public.city (id, name)
+VALUES (DEFAULT, 'Sillamäe');
 
-INSERT INTO public.size (id, type, price) VALUES (DEFAULT, 'Small(S)', 5);
-INSERT INTO public.size (id, type, price) VALUES (DEFAULT, 'Medium(M)', 7);
-INSERT INTO public.size (id, type, price) VALUES (DEFAULT, 'Large(L)', 10);
-INSERT INTO public.size (id, type, price) VALUES (DEFAULT, 'Extra Large(XL)', 13);
+INSERT INTO public.size (id, type, price)
+VALUES (DEFAULT, 'Small(S)', 5);
+INSERT INTO public.size (id, type, price)
+VALUES (DEFAULT, 'Medium(M)', 7);
+INSERT INTO public.size (id, type, price)
+VALUES (DEFAULT, 'Large(L)', 10);
+INSERT INTO public.size (id, type, price)
+VALUES (DEFAULT, 'Extra Large(XL)', 13);
 
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Akita');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Australian Shepherd');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Basset Hound');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Beagle');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Belgian Malinois');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Bernese Mountain Dog');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Bichon Frise');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Bloodhound');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Border Collie');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Boston Terrier');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Boxer');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Brittany');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Bulldog');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Cane Corso');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Cavalier King Charle');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Chesapeake Bay Retri');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Chihuahua');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Cocker Spaniel');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Collie');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Dachshund');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Doberman Pinscher');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'English Springer Spa');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'French Bulldog');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'German Shepherd');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'German Shorthaired P');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Golden Retriever');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Great Dane');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Havanese');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Labrador Retriever');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Maltese');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Mastiff');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Miniature American S');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Miniature Schnauzer');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Newfoundland');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Pembroke Welsh Corgi');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Pomeranian');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Poodle');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Pug');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Rhodesian Ridgeback');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Rottweiler');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Saint Bernard');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Shetland Sheepdog');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Shiba Inu');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Shih Tzu');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Siberian Husky');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Soft-Coated Wheaten');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Vizsla');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Weimaraner');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'West Highland White');
-INSERT INTO public.breed (id, name) VALUES (DEFAULT, 'Yorkshire Terrier');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Akita');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Australian Shepherd');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Basset Hound');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Beagle');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Belgian Malinois');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Bernese Mountain Dog');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Bichon Frise');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Bloodhound');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Border Collie');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Boston Terrier');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Boxer');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Brittany');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Bulldog');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Cane Corso');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Cavalier King Charle');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Chesapeake Bay Retri');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Chihuahua');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Cocker Spaniel');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Collie');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Dachshund');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Doberman Pinscher');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'English Springer Spa');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'French Bulldog');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'German Shepherd');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'German Shorthaired P');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Golden Retriever');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Great Dane');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Havanese');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Labrador Retriever');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Maltese');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Mastiff');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Miniature American S');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Miniature Schnauzer');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Newfoundland');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Pembroke Welsh Corgi');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Pomeranian');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Poodle');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Pug');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Rhodesian Ridgeback');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Rottweiler');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Saint Bernard');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Shetland Sheepdog');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Shiba Inu');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Shih Tzu');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Siberian Husky');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Soft-Coated Wheaten');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Vizsla');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Weimaraner');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'West Highland White');
+INSERT INTO public.breed (id, name)
+VALUES (DEFAULT, 'Yorkshire Terrier');
--- database/doge_create.sql
+++ database/doge_create.sql
@@ -3,256 +3,286 @@
 
 -- tables
 -- Table: breed
-CREATE TABLE breed (
-                       id serial  NOT NULL,
-                       name varchar(20)  NOT NULL,
-                       CONSTRAINT breed_pk PRIMARY KEY (id)
+CREATE TABLE breed
+(
+    id   serial      NOT NULL,
+    name varchar(20) NOT NULL,
+    CONSTRAINT breed_pk PRIMARY KEY (id)
 );
 
 -- Table: city
-CREATE TABLE city (
-                      id serial  NOT NULL,
-                      name varchar(255)  NOT NULL,
-                      CONSTRAINT city_pk PRIMARY KEY (id)
+CREATE TABLE city
+(
+    id   serial       NOT NULL,
+    name varchar(255) NOT NULL,
+    CONSTRAINT city_pk PRIMARY KEY (id)
 );
 
 -- Table: contact
-CREATE TABLE contact (
-                         id serial  NOT NULL,
-                         e_mail varchar(30)  NOT NULL,
-                         city varchar(30)  NOT NULL,
-                         firstname varchar(30)  NOT NULL,
-                         lastname varchar(30)  NOT NULL,
-                         adittion_information varchar(200)  NULL,
-                         photo_data bytea  NULL,
-                         CONSTRAINT contact_pk PRIMARY KEY (id)
+CREATE TABLE contact
+(
+    id                   serial       NOT NULL,
+    e_mail               varchar(30)  NOT NULL,
+    city                 varchar(30)  NOT NULL,
+    firstname            varchar(30)  NOT NULL,
+    lastname             varchar(30)  NOT NULL,
+    adittion_information varchar(200) NULL,
+    photo_data           bytea        NULL,
+    CONSTRAINT contact_pk PRIMARY KEY (id)
 );
 
 -- Table: dog
-CREATE TABLE dog (
-                     id serial  NOT NULL,
-                     owner_user_id int  NOT NULL,
-                     name varchar(30)  NOT NULL,
-                     age int  NOT NULL,
-                     adittional_information varchar(300)  NULL,
-                     CONSTRAINT dog_pk PRIMARY KEY (id)
+CREATE TABLE dog
+(
+    id                     serial       NOT NULL,
+    owner_user_id          int          NOT NULL,
+    name                   varchar(30)  NOT NULL,
+    age                    int          NOT NULL,
+    adittional_information varchar(300) NULL,
+    CONSTRAINT dog_pk PRIMARY KEY (id)
 );
 
 -- Table: dog_breed
-CREATE TABLE dog_breed (
-                           id serial  NOT NULL,
-                           dog_id int  NOT NULL,
-                           breed_id int  NOT NULL,
-                           CONSTRAINT dog_breed_pk PRIMARY KEY (id)
+CREATE TABLE dog_breed
+(
+    id       serial NOT NULL,
+    dog_id   int    NOT NULL,
+    breed_id int    NOT NULL,
+    CONSTRAINT dog_breed_pk PRIMARY KEY (id)
 );
 
 -- Table: dog_order
-CREATE TABLE dog_order (
-                           id serial  NOT NULL,
-                           dog_id int  NOT NULL,
-                           order_id int  NOT NULL,
-                           CONSTRAINT dog_order_pk PRIMARY KEY (id)
+CREATE TABLE dog_order
+(
+    id       serial NOT NULL,
+    dog_id   int    NOT NULL,
+    order_id int    NOT NULL,
+    CONSTRAINT dog_order_pk PRIMARY KEY (id)
 );
 
 -- Table: dog_picture
-CREATE TABLE dog_picture (
-                             id serial  NOT NULL,
-                             dog_id int  NOT NULL,
-                             picture_id int  NOT NULL,
-                             CONSTRAINT dog_picture_pk PRIMARY KEY (id)
+CREATE TABLE dog_picture
+(
+    id         serial NOT NULL,
+    dog_id     int    NOT NULL,
+    picture_id int    NOT NULL,
+    CONSTRAINT dog_picture_pk PRIMARY KEY (id)
 );
 
 -- Table: order
-CREATE TABLE "order" (
-                         id serial  NOT NULL,
-                         walking_id int  NOT NULL,
-                         walking_date date  NOT NULL,
-                         time_from time  NOT NULL,
-                         time_to time  NOT NULL,
-                         address int  NOT NULL,
-                         total_price int  NOT NULL,
-                         CONSTRAINT order_pk PRIMARY KEY (id)
+CREATE TABLE "order"
+(
+    id           serial NOT NULL,
+    walking_id   int    NOT NULL,
+    walking_date date   NOT NULL,
+    time_from    time   NOT NULL,
+    time_to      time   NOT NULL,
+    address      int    NOT NULL,
+    total_price  int    NOT NULL,
+    CONSTRAINT order_pk PRIMARY KEY (id)
 );
 
 -- Table: picture
-CREATE TABLE picture (
-                         id serial  NOT NULL,
-                         data bytea  NULL,
-                         CONSTRAINT picture_pk PRIMARY KEY (id)
+CREATE TABLE picture
+(
+    id   serial NOT NULL,
+    data bytea  NULL,
+    CONSTRAINT picture_pk PRIMARY KEY (id)
 );
 
 -- Table: price
-CREATE TABLE price (
-                       id serial  NOT NULL,
-                       size_id int  NOT NULL,
-                       hour int  NOT NULL,
-                       CONSTRAINT price_pk PRIMARY KEY (id)
+CREATE TABLE price
+(
+    id      serial NOT NULL,
+    size_id int    NOT NULL,
+    hour    int    NOT NULL,
+    CONSTRAINT price_pk PRIMARY KEY (id)
 );
 
 -- Table: role
-CREATE TABLE role (
-                      id serial  NOT NULL,
-                      type varchar(20)  NOT NULL,
-                      CONSTRAINT role_pk PRIMARY KEY (id)
+CREATE TABLE role
+(
+    id   serial      NOT NULL,
+    type varchar(20) NOT NULL,
+    CONSTRAINT role_pk PRIMARY KEY (id)
 );
 
 -- Table: size
-CREATE TABLE size (
-                      id serial  NOT NULL,
-                      type varchar(30)  NOT NULL,
-                      price int  NOT NULL,
-                      CONSTRAINT size_pk PRIMARY KEY (id)
+CREATE TABLE size
+(
+    id    serial      NOT NULL,
+    type  varchar(30) NOT NULL,
+    price int         NOT NULL,
+    CONSTRAINT size_pk PRIMARY KEY (id)
 );
 
 -- Table: user
-CREATE TABLE "user" (
-                        id serial  NOT NULL,
-                        role_id int  NOT NULL,
-                        contact_id int  NOT NULL,
-                        username varchar(20)  NOT NULL,
-                        password int  NOT NULL,
-                        CONSTRAINT user_pk PRIMARY KEY (id)
+CREATE TABLE "user"
+(
+    id         serial      NOT NULL,
+    role_id    int         NOT NULL,
+    contact_id int         NOT NULL,
+    username   varchar(20) NOT NULL,
+    password   int         NOT NULL,
+    CONSTRAINT user_pk PRIMARY KEY (id)
 );
 
 -- Table: walking
-CREATE TABLE walking (
-                         id serial  NOT NULL,
-                         walker_user_id int  NOT NULL,
-                         city_id int  NOT NULL,
-                         date_from date  NOT NULL,
-                         date_to date  NOT NULL,
-                         time_from time  NOT NULL,
-                         time_to time  NOT NULL,
-                         CONSTRAINT walking_pk PRIMARY KEY (id)
+CREATE TABLE walking
+(
+    id             serial NOT NULL,
+    walker_user_id int    NOT NULL,
+    city_id        int    NOT NULL,
+    date_from      date   NOT NULL,
+    date_to        date   NOT NULL,
+    time_from      time   NOT NULL,
+    time_to        time   NOT NULL,
+    CONSTRAINT walking_pk PRIMARY KEY (id)
 );
 
 -- Table: walking_size
-CREATE TABLE walking_size (
-                              id serial  NOT NULL,
-                              walking_id int  NOT NULL,
-                              size_id int  NOT NULL,
-                              CONSTRAINT walking_size_pk PRIMARY KEY (id)
+CREATE TABLE walking_size
+(
+    id         serial NOT NULL,
+    walking_id int    NOT NULL,
+    size_id    int    NOT NULL,
+    CONSTRAINT walking_size_pk PRIMARY KEY (id)
 );
 
 -- foreign keys
 -- Reference: dog_breed_breed (table: dog_breed)
-ALTER TABLE dog_breed ADD CONSTRAINT dog_breed_breed
-    FOREIGN KEY (breed_id)
-        REFERENCES breed (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE dog_breed
+    ADD CONSTRAINT dog_breed_breed
+        FOREIGN KEY (breed_id)
+            REFERENCES breed (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: dog_breed_dog (table: dog_breed)
-ALTER TABLE dog_breed ADD CONSTRAINT dog_breed_dog
-    FOREIGN KEY (dog_id)
-        REFERENCES dog (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE dog_breed
+    ADD CONSTRAINT dog_breed_dog
+        FOREIGN KEY (dog_id)
+            REFERENCES dog (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: dog_order_dog (table: dog_order)
-ALTER TABLE dog_order ADD CONSTRAINT dog_order_dog
-    FOREIGN KEY (dog_id)
-        REFERENCES dog (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE dog_order
+    ADD CONSTRAINT dog_order_dog
+        FOREIGN KEY (dog_id)
+            REFERENCES dog (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: dog_order_order (table: dog_order)
-ALTER TABLE dog_order ADD CONSTRAINT dog_order_order
-    FOREIGN KEY (order_id)
-        REFERENCES "order" (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE dog_order
+    ADD CONSTRAINT dog_order_order
+        FOREIGN KEY (order_id)
+            REFERENCES "order" (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: dog_picture_dog (table: dog_picture)
-ALTER TABLE dog_picture ADD CONSTRAINT dog_picture_dog
-    FOREIGN KEY (dog_id)
-        REFERENCES dog (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE dog_picture
+    ADD CONSTRAINT dog_picture_dog
+        FOREIGN KEY (dog_id)
+            REFERENCES dog (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: dog_picture_picture (table: dog_picture)
-ALTER TABLE dog_picture ADD CONSTRAINT dog_picture_picture
-    FOREIGN KEY (picture_id)
-        REFERENCES picture (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE dog_picture
+    ADD CONSTRAINT dog_picture_picture
+        FOREIGN KEY (picture_id)
+            REFERENCES picture (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: dog_user (table: dog)
-ALTER TABLE dog ADD CONSTRAINT dog_user
-    FOREIGN KEY (owner_user_id)
-        REFERENCES "user" (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE dog
+    ADD CONSTRAINT dog_user
+        FOREIGN KEY (owner_user_id)
+            REFERENCES "user" (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: order_walking (table: order)
-ALTER TABLE "order" ADD CONSTRAINT order_walking
-    FOREIGN KEY (walking_id)
-        REFERENCES walking (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE "order"
+    ADD CONSTRAINT order_walking
+        FOREIGN KEY (walking_id)
+            REFERENCES walking (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: price_size (table: price)
-ALTER TABLE price ADD CONSTRAINT price_size
-    FOREIGN KEY (size_id)
-        REFERENCES size (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE price
+    ADD CONSTRAINT price_size
+        FOREIGN KEY (size_id)
+            REFERENCES size (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: user_contact (table: user)
-ALTER TABLE "user" ADD CONSTRAINT user_contact
-    FOREIGN KEY (contact_id)
-        REFERENCES contact (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE "user"
+    ADD CONSTRAINT user_contact
+        FOREIGN KEY (contact_id)
+            REFERENCES contact (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: user_role (table: user)
-ALTER TABLE "user" ADD CONSTRAINT user_role
-    FOREIGN KEY (role_id)
-        REFERENCES role (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE "user"
+    ADD CONSTRAINT user_role
+        FOREIGN KEY (role_id)
+            REFERENCES role (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: walking_city (table: walking)
-ALTER TABLE walking ADD CONSTRAINT walking_city
-    FOREIGN KEY (city_id)
-        REFERENCES city (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE walking
+    ADD CONSTRAINT walking_city
+        FOREIGN KEY (city_id)
+            REFERENCES city (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: walking_size_size (table: walking_size)
-ALTER TABLE walking_size ADD CONSTRAINT walking_size_size
-    FOREIGN KEY (size_id)
-        REFERENCES size (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE walking_size
+    ADD CONSTRAINT walking_size_size
+        FOREIGN KEY (size_id)
+            REFERENCES size (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: walking_size_walking (table: walking_size)
-ALTER TABLE walking_size ADD CONSTRAINT walking_size_walking
-    FOREIGN KEY (walking_id)
-        REFERENCES walking (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE walking_size
+    ADD CONSTRAINT walking_size_walking
+        FOREIGN KEY (walking_id)
+            REFERENCES walking (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- Reference: walking_user (table: walking)
-ALTER TABLE walking ADD CONSTRAINT walking_user
-    FOREIGN KEY (walker_user_id)
-        REFERENCES "user" (id)
-        NOT DEFERRABLE
-            INITIALLY IMMEDIATE
+ALTER TABLE walking
+    ADD CONSTRAINT walking_user
+        FOREIGN KEY (walker_user_id)
+            REFERENCES "user" (id)
+            NOT DEFERRABLE
+                INITIALLY IMMEDIATE
 ;
 
 -- End of file.
